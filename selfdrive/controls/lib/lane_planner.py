98e136e3b905c076064f1cf1fe5bbadd7e1a6f62
eyezenheim <49458310+eyezenheim@users.noreply.github.com>
2022-04-02 05:41:35 -0700

    Intentional Right bias when in Lanefull

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -10,10 +10,10 @@ from selfdrive.swaglog import cloudlog
 TRAJECTORY_SIZE = 33
 # camera offset is meters from center car to camera
 if EON:
-  CAMERA_OFFSET = 0.00
+  CAMERA_OFFSET = -0.23
   PATH_OFFSET = 0.00
 elif TICI:
-  CAMERA_OFFSET = -0.10
+  CAMERA_OFFSET = -0.33
   PATH_OFFSET = -0.05
 else:
   CAMERA_OFFSET = 0.0
