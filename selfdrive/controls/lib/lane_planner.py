15ac202196813b2eaa377348d86a0caf92193950
eyezenheim <49458310+eyezenheim@users.noreply.github.com>
2022-04-25 10:34:45 -0700

    Desired Right Hug  on Lanefull Mode

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -10,10 +10,10 @@ from selfdrive.swaglog import cloudlog
 TRAJECTORY_SIZE = 33
 # camera offset is meters from center car to camera
 if EON:
-  CAMERA_OFFSET = 0.06
+  CAMERA_OFFSET = -0.53
   PATH_OFFSET = 0.00
 elif TICI:
-  CAMERA_OFFSET = -0.04
+  CAMERA_OFFSET = -0.53
   PATH_OFFSET = -0.00
 else:
   CAMERA_OFFSET = 0.0
